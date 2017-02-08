59f94758b24075224b6569376b98eded46994752
Thomas Leupp <tomleupp@gmail.com>
2017-02-07 22:53:44 -0800

    removed unused npm packages

--- android/app/src/main/java/com/fencer/MainApplication.java
+++ android/app/src/main/java/com/fencer/MainApplication.java
@@ -13,13 +13,10 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 
-import com.rt2zz.reactnativecontacts.ReactNativeContacts;
 import com.airbnb.android.react.maps.MapsPackage;
 import fr.greweb.reactnativeviewshot.RNViewShotPackage;
 import cl.json.RNSharePackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
-//import com.wix.RNCameraKit.RNCameraKitPackage;
-import com.github.alinz.reactnativewebviewbridge.WebViewBridgePackage;
 import com.surialabs.rn.geofencing.GeoFencingPackage;
 
 import java.util.Arrays;
@@ -37,17 +34,14 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage(),
-            new RNFetchBlobPackage(),
-            new ImageResizerPackage(),
+          new VectorIconsPackage(),
+          new RNFetchBlobPackage(),
+          new ImageResizerPackage(),
           new ImagePickerPackage(),
           new MapsPackage(),
-          new ReactNativeContacts(),
           new RNSharePackage(),
           new RNViewShotPackage(),
           new RCTCameraPackage(),
-        //  new RNCameraKitPackage(), 
-          new WebViewBridgePackage(),
           new GeoFencingPackage()
       );
     }
